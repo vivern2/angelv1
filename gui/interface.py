01fad25b183f6e0b4510805e53465b24fa470f06
vivern2 <vivernart@gmail.com>
2026-03-19 18:00:49 -0500

    new

--- gui/interface.py
+++ gui/interface.py
@@ -1,3 +1,17 @@
 import customtkinter 
+
+#below creates the main app window
 app = customtkinter.CTk()
+
+#----------CUSTOMIZATION--------------------------------------
+#below sets the title of the app window
+app.title("Angel AI")
+#below sets the mode + theme of the app window
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")  
+#below sets the size of the app window
+app.geometry("400x300")
+#---------------------------------------------------------------
+
+#below starts the GUI loop
 app.mainloop()
