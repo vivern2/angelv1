ace1bb23549db99af93c85a14ca53f9d68f2281e
vivern2 <vivernart@gmail.com>
2026-03-23 10:00:12 -0500

    connecting openai

--- brain/ai.py
+++ brain/ai.py
@@ -1,11 +1,39 @@
+#below allows us to acess operating system features such as environment variables(env) and more
+import os
+#below connects acess to the python .env feature to use api keys saved there
+from dotenv import load_dotenv
+#below we are using openai for the actual ai for now
+from openai import OpenAI
+
+#below loads the environment variables
+load_dotenv()
+
+#creates open ai client with the specific api key
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
-#Below is a test of the function get_response() to connect it to the gui
-def get_response(text):
-    text = text.lower()
-    if "hello" in text:
-        return "Hello! I'm Angel. How can I assist you today?"
-    if "population of cuba?" in text:
-        return "The population of Cuba is approximately 11 million people."
-    
-    return "Sorry, I don't understand that yet. I'm still learning!"
+#Below is a function that will connect to the ai and give the ai's response
+def get_response(user_text):
+    try:
+        #below the response = client stuff creates request to AI
+        response = client.chat.completions.create(
+            #below will be the model we are using we can change later
+            model = "gpt-40-mini",
+
+            #below messages is how we talk to the AI
+            messages=[ 
+                #below defines who Angel is with a system message
+                {"role": "system", "content": "you are Angel, a helpful and polite AI assistant"},
+                #below is the user and what the user messages 
+                {"role": "user", "content": user_text}
+            ]
+        )
+
+        #below extracts the ai's answer text, basically the AI's output
+        return response.choices[0].message.content
+
+    #below is error handlign
+    except Exception as e:
+        #Below will print an error in the terminal
+        print("ERROR:", e)
+        return "Sorry, something went wrong."
