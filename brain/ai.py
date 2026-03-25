18150933a7f1d351a6345d64b55d3cf3f7ac46b2
vivern2 <vivernart@gmail.com>
2026-03-24 19:40:33 -0500

    removing openai and added ollama

--- brain/ai.py
+++ brain/ai.py
@@ -1,36 +1,14 @@
-#below allows us to acess operating system features such as environment variables(env) and more
-import os
-#below connects acess to the python .env feature to use api keys saved there
-from dotenv import load_dotenv
-#below we are using openai for the actual ai for now
-from openai import OpenAI
-
-#below loads the environment variables
-load_dotenv()
-
-#creates open ai client with the specific api key
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+import ollama 
 
 
 #Below is a function that will connect to the ai and give the ai's response
 def get_response(user_text):
     try:
-        #below the response = client stuff creates request to AI
-        response = client.chat.completions.create(
-            #below will be the model we are using we can change later
-            model = "gpt-4o-mini",
-
-            #below messages is how we talk to the AI
-            messages=[ 
-                #below defines who Angel is with a system message
-                {"role": "system", "content": "you are Angel, a helpful and polite AI assistant"},
-                #below is the user and what the user messages 
-                {"role": "user", "content": user_text}
-            ]
-        )
-
-        #below extracts the ai's answer text, basically the AI's output
-        return response.choices[0].message.content
+        #Below will connect to the ai and get the response
+        response = ollama.chat(
+            
+            )
+        return response
 
     #below is error handlign
     except Exception as e:
