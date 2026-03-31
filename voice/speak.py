082c909cb4627d5f2c6ca738f5bb9d6454ab67d5
vivern2 <vivernart@gmail.com>
2026-03-31 18:50:55 -0500

    new changes

--- voice/speak.py
+++ voice/speak.py
@@ -1,14 +1,17 @@
 #Speak.py controls how angel will speak
 
+#below allows us to run system level commands 
 import os
 
 #below imports the pyttsx3 library which is used for text to speech conversion
 import pyttsx3
 
+#Below allows us to run terminal commands safely 
 import subprocess
 
 #this function initializes the pyttsx3 engine and sets the voice properties
 engine = pyttsx3.init(driverName='espeak')
-#this function takes a string input and uses the pyttsx3 engine to speak it out loud
+
+#Below is the speak function that takes in text as an argument and uses the pyttsx3 engine to convert it to speech
 def speak(text):
      subprocess.run(["espeak-ng", text])
